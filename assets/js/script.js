e3df080949911a250edfffa23b5006363469efc2
Natalia Mellado <nataliamelladocani@gmail.com>
2024-02-09 08:39:02 -0300

    Update structure in index.html and style.css.

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+function cargarContenido(url) {
+    // Utiliza la Fetch API para cargar el contenido
+    fetch(url)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.text();
+        })
+        .then(data => {
+            // Cuando la carga es exitosa, actualiza el contenido
+            document.getElementById('contenido').innerHTML = data;
+        })
+        .catch(error => {
+            console.error('Error fetching content:', error);
+        });
+}
